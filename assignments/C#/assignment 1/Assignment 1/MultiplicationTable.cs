271c327b776eb03b523b6abe048d235b11abb17e
Praveen Dhamaraj <praveend@infinite.com>
2026-03-26 12:21:04 +0530

    Added Multiplication Table file.

---
+++ assignments/C#/assignment 1/Assignment 1/MultiplicationTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class MultipicationTable
+    {
+        public static void Tables()
+        {
+            int table;
+            Console.Write("Enter the number : ");
+            table = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= 10; num++)
+            {
+                Console.WriteLine($"{table} X {num} = {table * num}");
+            }
+        }
+    }
+}
