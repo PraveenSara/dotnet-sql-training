1a6a4c047f50eb8f24adeb67397fd16b8f11aaf6
Praveen Dharmaraj <praveend@infinite.com>
2026-04-06 22:29:26 +0530

    Pushed 1nd and 2nd task from Assignment 6.

---
+++ assignments/C#/Assignment 6/Assignment 6/Books.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    internal class Books
+    {
+        string BookName, AuthorName;
+        
+        public Books(string BName, string AName)
+        {
+            BookName = BName;
+            AuthorName = AName;
+        }
+        internal void Display()
+        {
+            Console.WriteLine("Book : " + BookName + " Author : " + AuthorName);
+        }
+    }
+    internal class BookShelf
+    {
+        Books[] books = new Books[5];
+        public Books this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+    }
+}
---
+++ assignments/C#/Assignment 6/Assignment 6/FileArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+    internal class FileArray
+    {
+        public void Array()
+        {
+            string[] todo = new string[3] { "Wakeup", "C# Assignment", "Eat fruits" };
+
+
+            string Path = @"C:\praveen-dotnet-sql-training\assignments\C#\Assignment 6\Assignment 6\todo_Array.txt";
+            File.WriteAllLines(Path, todo);
+
+            Console.WriteLine("File created successfully..  ");
+
+            string[] ReadData = File.ReadAllLines(Path);
+
+            Console.WriteLine("Reading from file\n");
+            foreach (string line in ReadData)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
