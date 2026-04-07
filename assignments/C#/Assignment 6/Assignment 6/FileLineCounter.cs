3f790708975ba387a3f758eb4e9fa77c9778e4bf
Praveen Dharmaraj <praveend@infinite.com>
2026-04-07 08:29:45 +0530

    Pushed Assignment 6.

---
+++ assignments/C#/Assignment 6/Assignment 6/FileLineCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_6
+{
+    internal class FileLineCounter
+    {
+        internal void LineCount()
+        {
+            string path = @"C:\praveen-dotnet-sql-training\assignments\C#\Assignment 6\Assignment 6\todo_Array.txt";
+
+            if (File.Exists(path))
+            {
+                int Lines = File.ReadLines(path).Count();
+                Console.WriteLine("Number of lines in this file : "+ Lines);
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
--- assignments/C#/Assignment 6/Assignment 6/Program.cs
+++ assignments/C#/Assignment 6/Assignment 6/Program.cs
@@ -10,22 +10,27 @@ namespace Assignment_6
     {
         static void Main(string[] args)
         {
-            //BookShelf bs = new BookShelf();
-            //bs[0] = new Books("Psychology of money", "Morgen Housel");
-            //bs[1] = new Books("Atomic habits", "James clear");
-            //bs[2] = new Books("How to win friends and influence people", "Dale carnegie");
-            //bs[3] = new Books("Can we be strangers again", "Sreejit shandalia");
-            //bs[4] = new Books("Rich dad poor dad", "Robert Kyotsaki");
+            BookShelf bs = new BookShelf();
+            bs[0] = new Books("Psychology of money", "Morgen Housel");
+            bs[1] = new Books("Atomic habits", "James clear");
+            bs[2] = new Books("How to win friends and influence people", "Dale carnegie");
+            bs[3] = new Books("Can we be strangers again", "Sreejit shandalia");
+            bs[4] = new Books("Rich dad poor dad", "Robert Kyotsaki");
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    bs[i].Display();
-            //}
+            for (int i = 0; i < 5; i++)
+            {
+                bs[i].Display();
+            }
 
-            // 2nd program
+            //2nd program
 
             FileArray fa = new FileArray();
             fa.Array();
+
+            //3rd task
+
+            FileLineCounter fl = new FileLineCounter();
+            fl.LineCount();
             Console.Read();
         }
     }
